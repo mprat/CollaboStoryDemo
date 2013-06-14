1a73d8bc622e6bda1e9d38e867d924c56d1fcbde
mprat <michele.pratusevich@gmail.com>
2013-06-14 17:45:35 -0400

    first basic view that does nothing added

--- CollaboStory/settings.py
+++ CollaboStory/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/mprat/Documents/Michele/Repos/CollaboStory/collabostorydb',                      # Or path to database file if using sqlite3.
+        'NAME': '/home/mprat/Documents/Michele/Repos/CollaboStoryDemo/collabostorydb',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -111,6 +111,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    "/home/mprat/Documents/Michele/Repos/CollaboStoryDemo/templates",
 )
 
 INSTALLED_APPS = (
@@ -121,9 +122,10 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'collabostory',
 )
 
 # A sample logging configuration. The only tangible logging
